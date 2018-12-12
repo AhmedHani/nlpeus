fe2b9100a89c6c168b618c31b64d7d3cd1d1336e
Ahmed Hani <ahani.fcis@gmail.com>
2018-12-12 21:46:56 +0200

    update readme & adding scripts file

--- scripts.sh
+++ scripts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# running style recognition in floydub - CPU machine
+# running style recognition in floydhub - CPU machine
 floyd run --env pytorch-1.0 --follow "python3 -m style_recognition.research.local_driver"
