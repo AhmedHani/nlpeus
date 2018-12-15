b1678ed90aeeefae0969e0c13334b224db29a19c
Ahmed Hani <ahani.fcis@gmail.com>
2018-12-15 16:48:41 +0200

    add copyrights

--- common/transformations.py
+++ common/transformations.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2018-present, Ahmed H. Al-Ghidani.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+__author__ = "Ahmed H. Al-Ghidani"
+__copyright__ = "Copyright 2018, The nlpeus Project, https://github.com/AhmedHani/nlpeus"
+__license__ = "GPL"
+__maintainer__ = "Ahmed H. Al-Ghidani"
+__email__ = "ahmed.hani.ibrahim@gmail.com"
+
 import functools
 from utils.text_utils import Preprocessor
 
